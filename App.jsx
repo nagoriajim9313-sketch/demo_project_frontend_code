3dfa091b755453a14818fbd1b49ad1ad4780c943
nagoriajim9313-sketch <nagoriajim9313@gmail.com>
2026-05-25 09:57:01 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import AppRoute from './Routes/AppRoute'
+import ProductDisplay from './Page/OrderAll'
+function App() {
+  
+  return (
+    <>
+     <AppRoute/>
+    </>
+  )
+}
+
+export default App
